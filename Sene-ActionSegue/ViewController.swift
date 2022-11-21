e0414a644737dfe91557218d9f57ea9f6226adc0
nakrlove <nakrlove@gmail.com>
2022-11-21 19:35:11 +0900

    Initial Commit

---
+++ Sene-ActionSegue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sene-ActionSegue
+//
+//  Created by nakrlove on 2022/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
